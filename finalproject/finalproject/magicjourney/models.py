932d904dd76ea79cfe4f81b14e43ab5dca165b08
dylodylo <artur.dylewski.27@gmail.com>
2020-11-18 15:13:48 +0100

    Add Player model

--- finalproject/finalproject/magicjourney/models.py
+++ finalproject/finalproject/magicjourney/models.py
@@ -3,4 +3,21 @@ from django.db import models
 # Create your models here.
 
 class User(AbstractUser):
+    pass
+    
+class Player(models.Model):
+    blood_choices = [
+        ("muggle", "Muggle"),
+        ("half-blood", "Half-blood"),
+        ("pure-blood", "Pure-blood")
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     level = models.IntegerField(default=0)
+    blood = models.CharField(choices=blood_choices, blank=True, max_length=30)
+    hp = models.IntegerField(default=0)
+    defence = models.IntegerField(default=0)
+    charms = models.IntegerField(default=0)
+    transmutation = models.IntegerField(default=0)
+    potions = models.IntegerField(default=0)
+    charisma = models.IntegerField(default=0)
+    money = models.IntegerField(default=0) #in knuts
--- finalproject/finalproject/magicjourney/urls.py
+++ finalproject/finalproject/magicjourney/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("logout", views.logout_view, name="logout"),
     path("login", views.login_view, name="login"),
-    path("/user/<str:username>", views.userpage, name="userpage"),
+    path("user/<str:username>", views.userpage, name="userpage"),
 ]
 
